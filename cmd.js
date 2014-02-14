59cf81480fbe58846d6f95b2abbd8556d39a904b
James Halliday <mail@substack.net>
2014-02-13 18:59:08 -0800

    empty stub

---
+++ cmd.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+var workshopper = require('workshopper');
+
+var w = workshopper({
+    name: 'browserify-adventure',
+    title: 'BROWSERIFY ADVENTURE',
+    appDir: __dirname
+});
+w.init();
