596716a8d5d1996c69bb2b600f2fbecf9b6ae093
James Halliday <mail@substack.net>
2014-07-02 18:08:25 -0700

    more tests for builtins

--- problems/builtins/index.js
+++ problems/builtins/index.js
@@ -7,36 +7,50 @@ exports.problem = fs.createReadStream(__dirname + '/problem.txt');
 exports.solution = fs.createReadStream(__dirname + '/solution.txt');
 
 exports.verify = verify({ modeReset: true }, function (args, t) {
-    t.plan(1);
+    t.plan(3);
     process.stdin.pipe(concat(function (body) {
         try { var rows = unpack(body) }
         catch (err) { return t.fail('The input had a syntax error!') }
         if (!rows) return t.fail('The input is not a browserify bundle!');
         
         var expected = [
-            'http://substack.net/filez/hi/doge.gif'
+            'http://substack.net/filez/hi/doge.gif',
+            'http://lebron.technology/lebron.jpeg',
+            'https://fbi.gov/classified/x-files/swamp_monster.pdf'
         ];
         run(body, function (res) {
-            t.equal(res, expected.shift(), 'expected result');
+            t.equal(res.output[0], expected.shift(), res.input);
         });
     }));
 });
 
 exports.run = function (args) {
     process.stdin.pipe(concat(function (body) {
-        run(body, function (res) { console.log(res) });
+        run(body, function (res) {
+            console.log('INPUT: ' + JSON.stringify(res.input));
+            console.log('OUTPUT: ' + JSON.stringify(res.output));
+            console.log();
+        });
     }));
 };
 
 function run (body, cb) {
     var inputs = [
         'http://substack.net/filez/dogez/img.cgi?file=../hi/doge.gif',
+        'http://lebron.technology/nba/allstars?slamdunk=true&file=/lebron.jpeg',
+        'https://fbi.gov/classified/docs.php?file=x-files/swamp_monster.pdf&xtag=333',
     ];
+    var last = null;
     var con = {
-        log: function () { cb.apply(null, [].slice.call(arguments)) },
+        log: function () {
+            cb({ input: last, output: [].slice.call(arguments) })
+        },
         error: console.error
     };
-    var prompt = function () { return inputs.shift() };
+    var prompt = function () {
+        last = inputs.shift()
+        return last;
+    };
     var size = inputs.length;
     for (var i = 0; i < size; i++) {
         Function(['console','prompt'], body.toString())(con, prompt);
