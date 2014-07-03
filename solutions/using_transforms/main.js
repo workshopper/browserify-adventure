d62032913592df405d68be8772083ec9a05ad53a
James Halliday <mail@substack.net>
2014-07-02 18:59:26 -0700

    transforms example using sprintf

--- solutions/using_transforms/main.js
+++ solutions/using_transforms/main.js
@@ -1,16 +1,13 @@
 var fs = require('fs');
 var txt = fs.readFileSync(__dirname + '/../../problems/using_transforms/wake.txt', 'utf8');
+var printf = require('printf');
 
 var lines = txt.split('\n');
 lines.forEach(function (line, index) {
     if (index % 5 === 0) {
-        console.log(pad(String(index), 3) + ' ' + line);
+        console.log(printf('%3d %s', index, line));
     }
     else {
         console.log('    ' + line);
     }
 });
-
-function pad (s, n) {
-    return Array(Math.max(0,n-s.length+1)).join(' ') + s;
-}
