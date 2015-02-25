8a43ad72a7f2b877c1c394b45c0550c04f37da0e
Harry Moreno <morenoh149@gmail.com>
2015-02-24 17:48:39 -0800

    update  to use sprintf-js

--- solutions/using_transforms/main.js
+++ solutions/using_transforms/main.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var txt = fs.readFileSync(__dirname + '/../../problems/using_transforms/wake.txt', 'utf8');
-var sprintf = require('sprintf');
+var sprintf = require('sprintf-js').sprintf;
 
 var lines = txt.split('\n');
 lines.forEach(function (line, index) {
